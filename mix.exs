f0aecb9fecdd9119969ecf96657ec2f8dc0be0b4
Eli Knebel <eliknebel@gmail.com>
2021-02-25 13:18:13 -0500

    upversion

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lti1p3EctoProvider.MixProject do
   def project do
     [
       app: :lti_1p3_ecto_provider,
-      version: "0.1.3",
+      version: "0.2.0",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: elixirc_options(Mix.env()),
