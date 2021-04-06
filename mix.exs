c1266cd19a3a8421163b9e8b092dce94ea08ebbd
Eli Knebel <eliknebel@cmu.edu>
2021-04-06 14:10:07 -0400

    fix version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lti1p3EctoProvider.MixProject do
   def project do
     [
       app: :lti_1p3_ecto_provider,
-      version: "0.1.4",
+      version: "0.2.1",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: elixirc_options(Mix.env()),
