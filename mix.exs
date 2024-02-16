dfe862741022e6fe7ff089a030a1d3adf244c758
Eli Knebel <eliknebel@cmu.edu>
2024-02-16 16:02:35 -0500

    Update version to 0.6.0 for lti_1p3 package

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lti1p3EctoProvider.MixProject do
   def project do
     [
       app: :lti_1p3_ecto_provider,
-      version: "0.5.0",
+      version: "0.6.0",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: elixirc_options(Mix.env()),
@@ -43,7 +43,7 @@ defmodule Lti1p3EctoProvider.MixProject do
       {:ex_doc, "~> 0.23", only: :dev, runtime: false},
       {:ecto_sql, "~> 3.10"},
       {:httpoison, "~> 2.0"},
-      {:lti_1p3, "~> 0.5.0"},
+      {:lti_1p3, "~> 0.6.0"},
       {:mox, "~> 0.5", only: :test},
       {:postgrex, ">= 0.0.0"},
       {:timex, "~> 3.5"},
